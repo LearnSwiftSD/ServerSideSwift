3f80fb860e016c6e0bc49156fa2d8628d1ec5670
Stephen Martinez <enharmonicnuance@gmail.com>
2019-02-21 18:10:19 -0800

    registered the remaining routes

--- SwiftCastRank/Sources/App/routes.swift
+++ SwiftCastRank/Sources/App/routes.swift
@@ -3,9 +3,10 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     
-    #warning("TODO: Setup your HostController and register it")
     let swiftCastController = SwiftCastController()
+    let hostController = HostController()
     
     try router.register(collection: swiftCastController)
+    try router.register(collection: hostController)
     
 }
